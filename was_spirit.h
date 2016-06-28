e87df4b991315ad6fc0252674ed30b076edc7958
dingjc <dingjc.mchz.com.cn>
2016-06-28 18:02:08 +0800

    c

--- was_spirit.h
+++ was_spirit.h
@@ -1,6 +1,78 @@
 #ifndef __CPPUTILS_WASFILE
 #define __CPPUTILS_WASFILE
 #include <string.h>
+#include <assert.h>
+
+namespace dream {
+	
+class Data {
+public:
+
+	Data(unsigned char* buf,size_t size):buf(_buffer),_pos(0),_buffer_end(buf + size){}
+	
+	virtual ~Data() {
+		if(_buffer) {
+			delete[] _buffer;
+			_buffer = NULL;
+		}
+	}
+	
+	unsigned char read_byte() {
+		unsigned char ret = *(unsigned char*)(_buffer + _pos);
+		_pos = _pos + 1;
+		
+		assert(_buffer + _pos <= _buffer_end);
+		
+		return ret;
+	}
+	
+	unsigned short read_ushort() {
+		unsigned short ret = *(unsigned short*)(_buffer + _pos);
+		_pos = _pos + 2;
+		
+		assert(_buffer + _pos <= _buffer_end);
+		
+		return ret;
+	}
+	
+	int read_int() {
+		int ret = *(int*)(_buffer + _pos);
+		_pos = _pos + 4;
+		
+		assert(_buffer + _pos <= _buffer_end);
+		
+		return ret;
+	}
+	
+	unsigned int read_uint() {
+		unsigned int ret = *(unsigned int*)(_buffer + _pos);
+		_pos = _pos + 4;
+		
+		assert(_buffer + _pos <= _buffer_end);
+		
+		return ret;
+	}
+	
+	void read(unsigned char* buf,size_t size) {
+		
+		assert(_buffer + _pos + size <= _buffer_end);
+		
+		memcpy(buf,_buffer + _pos,size);
+		
+		_pos = _pos + size;
+	}
+	
+	void seek(size_t pos) {
+		_pos = pos;
+		
+		assert(_buffer + _pos <= _buffer_end);
+	}
+	
+private:
+	unsigned char* _buffer;
+	size_t _pos;
+	unsigned char* _buffer_end;
+};
 
 class WasSpirit {
 public:
@@ -90,5 +162,8 @@ private:
 	WasHead _wasHead;
 	Frame** _frames;
 };
+
+};
+
 
 #endif
